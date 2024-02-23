fe568cff9ee7d6648b0f623666a5c4c3330c6137
niezgodam <michalniezgoda100@gmail.com>
2024-02-23 19:17:10 +0100

    fixing path problem

--- LearningProgramme/program.py
+++ LearningProgramme/program.py
@@ -6,14 +6,21 @@ load_dotenv()
 
 
 file_name = os.getenv('FILENAME')
+
+if not file_name:
+    print("FILENAME environment variable is not set.")
+    sys.exit(1)
+
 with open(file_name, "r", encoding="utf-8") as f:
     lines = f.readlines()
 
 
 questions = lines[0::2]  
 answers = lines[1::2]  
+print("Press enter to see answer")
+print()
 
-while questions:
+while questions and answers:
 
     index = random.randint(0, len(questions)-1)
     if questions[index]:
