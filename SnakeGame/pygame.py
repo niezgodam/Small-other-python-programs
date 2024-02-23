edd9af33a6c8204133f62be131285c45d73f8eef
niezgodam <michalniezgoda100@gmail.com>
2024-02-23 17:54:09 +0100

    removal of unnecessary lines

--- SnakeGame/pygame.py
+++ SnakeGame/pygame.py
@@ -145,7 +145,6 @@ screen_height = window.winfo_screenheight()
 x = int((screen_width/2)-(window_width/2))
 y = int((screen_height/2)-(window_height/2))
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
-print("jsadkahsd"+str(window_width))
 
 
 
