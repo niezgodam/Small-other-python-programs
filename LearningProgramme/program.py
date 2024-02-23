a9bf12e93eadd6d07e2a116f2b81ea70975a851c
niezgodam <michalniezgoda100@gmail.com>
2024-02-23 18:24:49 +0100

    update program

--- LearningProgramme/program.py
+++ LearningProgramme/program.py
@@ -1,23 +1,32 @@
 import random
+import sys
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
-with open("pytania_odpowiedzi.txt", "r", encoding="utf-8") as f:
+file_name = os.getenv('FILENAME')
+with open(file_name, "r", encoding="utf-8") as f:
     lines = f.readlines()
 
 
 questions = lines[0::2]  
 answers = lines[1::2]  
 
-for i in range(len(questions)):
+while questions:
 
     index = random.randint(0, len(questions)-1)
-    pytanie = questions[index]
+    if questions[index]:
+        pytanie = questions[index]
+    else:
+        print("The questions are over")
+        sys.exit(0)
 
 
     input(f"{pytanie.strip()}")
 
-    odpowiedz = answers[index]
-    print(odpowiedz.strip())
+    answer = answers[index]
+    print(answer.strip())
     print("")
     answers.pop(index)
 
